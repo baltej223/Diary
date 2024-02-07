680a8876e7ceb611cdb5bd72385febef4f838752
baltej223 <73211864+baltej223@users.noreply.github.com>
2024-02-07 11:46:02 +0530

    Update js.js
    none lol

--- js.js
+++ js.js
@@ -1,3 +1,11 @@
+//one day i made a reader.html
+// it was just for reading what you have written in index.html as it had 'book like' UI and was much pleasent to use 
+// the data from index.html was shared through link to reader.html
+// the syntax was like /reader.html#everythin-written-here-will-be-shown-in-reader
+// for changin normal written text to dashed one i wrote this script
+// i dont know why by after some time i thought that concept of reader is completly pointless and then i deleted it from github and its source code but fortuantely i forgot to delete
+// this script from github and now i will use this script for future refrence
+// bye
 var href = window.location.href;
 var fhd = href.search("#");
 var tds = href.slice(fhd);
